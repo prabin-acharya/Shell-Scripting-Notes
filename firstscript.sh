233dce9c7b2595900f927edb71c0f6dd72b2c382
Prabin Acharya <acharyaprabin101@gmail.com>
2022-01-28 20:46:55 +0545

    variables, User Input, If-Else, Comparison

---
+++ firstscript.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+
+# chmod +x filename.sh
+# To add executable permission to the fille
+
+# ./filename.sh To run the file/script
+
+# ECHO COMMAND
+# echo Hello World!
+
+# VARIABLES
+# UPPERCASE BY CONVENTION
+# Letters, numbers, underscores
+# NAME="Prabin"
+# echo "My name is $NAME"
+# echo "My name is ${NAME}"
+
+# USER INPUT
+# read -p "ENter your name: " NAME2
+# echo "Hello, ${NAME2} nice to meet you!"
+
+# SIMPLE IF STATEMENT
+# NAME3="Pawan"
+# if [ "$NAME3" == "Pawan" ]
+# then
+#     echo "Your name is Pawan"
+# fi
+
+# IF-ELSE
+# NAME4="Arpan"
+# if [ "$NAME4" == "Arpann" ]
+# then
+#     echo "Your name is Arpan"
+# else
+#     echo "Your name is not Arpan"
+# fi
+
+
+# ELSE-IF(elif)
+# NAME5="Aaradhya"
+# if [ "$NAME5" == "Anup" ]
+# then
+#     echo "Your name is Anup"
+# elif [ "$NAME5" == "Aaradhya" ]
+# then
+#     echo "Your name is Aaradhya"
+# else
+#     echo "Your name is not Anup or Aaradhya"
+# fi
+
+
+# COMPARISON
+# -eq equal to
+# -ne not equal to
+# -gt greater than
+# -ge greater than or equal to
+# -lt less than
+# -le less than or equal to
+# NUM1=51
+# NUM2=7
+# if [ "$NUM1" -gt "$NUM2" ]
+# then 
+#     echo "$NUM1 is greater than $NUM2"
+# else
+#     echo "$NUM1 is less than $NUM2"
+# fi
+
+
