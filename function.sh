8fcbd0874778ed12430386d818296d3e211d0564
Prabin Acharya <acharyaprabin101@gmail.com>
2022-01-29 22:17:29 +0545

    Add function, example script to create a directory/file

---
+++ function.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+# FUNCTION
+# function sayHello() {
+#   echo "Hello World"
+# }
+# sayHello
+
+# FUNCTION WITH PARAMS
+function greet() {
+  echo "Hello, I am $1 and I am $2"
+}
+
+greet "Prabin" "19"
